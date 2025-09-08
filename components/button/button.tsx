2edcd45e22fb6f09cec1990040990b457e4e9b49
Brooklee Watters <brooklee.wilson@gmail.com>
2025-09-08 10:12:23 -0600

    update button animation

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -13,9 +13,10 @@ export function Button({ label }: ButtonProps) {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
   const svgRef = useRef<SVGSVGElement | null>(null);
+  const arrowPathRef = useRef<SVGPathElement | null>(null);
 
   useEffect(() => {
-    gsap.set([buttonRef.current, svgRef.current], { clearProps: "all" });
+    gsap.set([buttonRef.current, svgRef.current, arrowPathRef.current], { clearProps: "all" });
   }, []);
 
   const computeSwap = useCallback(() => {
@@ -39,12 +40,25 @@ export function Button({ label }: ButtonProps) {
     gsap.to([buttonRef.current, svgRef.current], { x: 0, duration: 0.2, ease: "power3.in" });
   }, []);
 
+  const handleSVGClick = useCallback(() => {
+    const arrow = arrowPathRef.current;
+    if (!arrow) return;
+
+    const distance = 300;
+    const tl = gsap.timeline();
+    tl.to(arrow, { x: distance, duration: 0.25, ease: "power3.in" })
+      .to(arrow, { opacity: 0}, "<")
+      .set(arrow, { x: -distance })
+      .to(arrow, { opacity: 1, duration: 0.05 })
+      .to(arrow, { x: 0, duration: 0.25, ease: "power3.out" });
+  }, []);
+
   return (
       <div className={styles.buttonContainer} ref={containerRef} onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
         <button ref={buttonRef} className={styles.button}>{label}</button>
-        <svg ref={svgRef} className={styles.arrow} width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M25.2486 32.5L31.4986 26.25M31.4986 26.25L25.2486 20M31.4986 26.25H19"/> // circle 
-          <path d="M11.4227 5.6875C15.5263 2.73737 20.5603 1 26 1C39.8071 1 51 12.1929 51 26C51 39.8071 39.8071 51 26 51C12.1929 51 1 39.8071 1 26C1 23.8419 1.27346 21.7476 1.78761 19.75" /> // arrow
+        <svg ref={svgRef} className={styles.arrow} width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleSVGClick}>
+          <path ref={arrowPathRef} d="M25.2486 32.5L31.4986 26.25M31.4986 26.25L25.2486 20M31.4986 26.25H19"/> // arrow 
+          <path d="M11.4227 5.6875C15.5263 2.73737 20.5603 1 26 1C39.8071 1 51 12.1929 51 26C51 39.8071 39.8071 51 26 51C12.1929 51 1 39.8071 1 26C1 23.8419 1.27346 21.7476 1.78761 19.75" /> // circle
         </svg>
       </div>
     );
